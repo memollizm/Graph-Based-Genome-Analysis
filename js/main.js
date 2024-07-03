93198b99b4487dc19f94369e06a00a8629363412
Mehmet Başgöze <mhmtbsgz34@gmail.com>
2024-07-04 02:47:00 +0300

    Model ve kamera düzenlendi

--- js/main.js
+++ js/main.js
@@ -31,7 +31,7 @@ controls.minDistance = 1;
 controls.maxDistance = 50;
 controls.minPolarAngle = 0.5;
 controls.maxPolarAngle = 1.5;
-controls.autoRotate = false; //Sürekli dönmesi için [true] yapılmalı
+controls.autoRotate = true; //Sürekli dönmesi için [true] yapılmalı
 controls.target = new THREE.Vector3(0, 15, 0);
 controls.update();
 
@@ -96,11 +96,43 @@ window.addEventListener('resize', () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-// Liste verileri
+// Liste verileri ve metin içerikleri
 const bodyParts = {
-  'Kafa': ['Ense', 'Burun', 'Kafatası', 'Kulak', 'Göz', 'Ağız'],
-  'Kol-Bilek-Parmak': ['Kol', 'Bilek', 'Parmak'],
-  'Vücut': ['Kalp', 'Akciğer', 'Karaciğer', 'Mide', 'Bağırsak', 'Üreme Organı', 'Bacak', 'Diz', 'Ön Ayak', 'Ayak']
+  'Kafa': {
+    subparts: ['Ense', 'Burun', 'Kafatası', 'Kulak', 'Göz', 'Ağız'],
+    content: 'Kafa bölgesi'
+  },
+  'Kol-Bilek-Parmak': {
+    subparts: ['Kol', 'Omuz', 'Bilek', 'Parmak'],
+    content: 'Kol-Bilek-Omuz-Parmak bölgesi'
+  },
+  'Vücut': {
+    subparts: ['Kalp', 'Akciğer', 'Karaciğer', 'Mide', 'Bağırsak', 'Üreme Organı', 'Bacak', 'Diz', 'Ön Ayak', 'Ayak'],
+    content: 'Vücut bölgesi'
+  }
+};
+
+const partDescriptions = {
+  'Ense': 'Migren, genetik faktörler, hormonal değişiklikler, stres ve çevresel faktörler gibi birçok nedene bağlı olabilir.',
+  'Burun': 'Sinüzit, sinüslerin iltihaplanması sonucu oluşabilir ve genellikle enfeksiyonlardan veya alerjilerden kaynaklanır.',
+  'Kafatası': 'Beyin tümörleri, hücrelerin kontrolsüz büyümesi sonucu oluşan kitlelerdir ve çeşitli faktörlere bağlı olabilir.',
+  'Kulak': 'İşitme kaybı, işitme organının veya işitme sinirinin hasar görmesi sonucu ortaya çıkan bir durumdur. Genetik faktörler, yaşlanma, yüksek ses seviyelerine maruz kalma, enfeksiyonlar, travma ve bazı ilaçlar işitme kaybına neden olabilir',
+  'Göz': 'Glokom, göz içindeki optik siniri etkileyen ve genellikle göz içi basıncının artmasıyla ilişkilendirilen bir grup göz hastalığıdır. Göz içi sıvısının dolaşımında bozukluklar veya drenaj problemleri glokoma neden olabilir ve tedavi edilmezse kalıcı görme kaybına yol açabilir.',
+  'Ağız': 'Aft, ağız içindeki yumuşak dokularda (yanaklar, dudaklar, dil, diş etleri) meydana gelen küçük, ağrılı yaralardır. Aftların nedeni tam olarak bilinmemekle birlikte, bağışıklık sistemi bozuklukları, stres, yaralanma, hormonal değişiklikler ve beslenme alışkanlıkları aft oluşumunu etkileyebilir.',
+  'Kol': 'Tenis dirseği, kolun dış kısmında bulunan tendonların iltihaplanması sonucu oluşan bir durumdur. Bu genellikle tekrarlayan el ve bilek hareketleri nedeniyle ortaya çıkar.',
+  'Omuz': 'Bursit, eklemi çevreleyen sıvı dolu keseciklerin (bursa) iltihaplanması sonucu oluşan bir durumdur. Eklem hareketlerinde sürtünmeyi azaltarak eklemi koruyan bu keseciklerin iltihaplanmasıyla ağrı, şişlik ve hareket kısıtlılığı ortaya çıkabilir. Bursit genellikle tekrarlayan travma, aşırı kullanım veya enfeksiyonlar sonucu gelişebilir.',
+  'Bilek': 'Karpal tünel sendromu, bilek bölgesindeki karpal tünelin sıkışması sonucu ortaya çıkan bir durumdur. Bu durum, el ve bilek bölgesinde ağrı, uyuşma, karıncalanma veya güçsüzlük hissi ile karakterizedir ve sıklıkla tekrarlayan el hareketleri veya bilek bölgesindeki tümörler nedeniyle oluşur.',
+  'Parmak': 'De Quervain tendiniti, el bileğinin tabanında yer alan tendonların iltihaplanması sonucu ortaya çıkan bir durumdur. Bu durum, baş parmak hareketleri sırasında ağrı, şişlik ve hassasiyet ile karakterizedir ve genellikle tekrarlayan baş parmak hareketleri, hamilelik veya romatoid artrit gibi durumlarla ilişkilendirilir.',
+  'Kalp': 'Koroner arter hastalığı, kalp kasına kan taşıyan koroner arterlerin daralması veya tıkanması sonucu oluşan bir durumdur. Bu durum, kalp krizi veya anjina gibi ciddi kalp problemlerine yol açabilir.',
+  'Akciğer': 'KOAH (Kronik Obstrüktif Akciğer Hastalığı), akciğerlerde hava yollarının kronik olarak daralması ve iltihaplanması sonucu oluşan bir durumdur. Bu durum, nefes alma güçlüğü, öksürük ve balgam üretimi gibi semptomlarla karakterizedir ve sigara içme gibi risk faktörleri ile ilişkilidir.',
+  'Karaciğer': 'Hepatit, karaciğerin iltihaplanması sonucu ortaya çıkan bir durumdur. Hepatit A, B, C, D ve E gibi farklı virüslerin neden olduğu viral hepatitler, alkol kullanımı veya bazı ilaçların neden olduğu toksik hepatitler gibi çeşitli tipleri vardır.',
+  'Mide': 'Helicobacter pylori, mide ve onikiparmak bağırsağı mukozasında enfeksiyona neden olan bir bakteridir. Helicobacter pylori enfeksiyonu, mide ülseri, gastrit ve mide kanseri gibi sindirim sistemi hastalıklarına yol açabilir.',
+  'Bağırsak': 'Kolon ve rektum polipleri, kalın bağırsakta (kolon) veya rektumda oluşan küçük tümörlerdir. Polipler genellikle kanser öncüsü değildir, ancak bazı polipler zamanla kansere dönüşebilir.',
+  'Üreme Organı': 'Frengi, Treponema pallidum adlı bakterinin neden olduğu cinsel yolla bulaşan bir enfeksiyon hastalığıdır. Frengi, cilt lezyonları, ülserler, lenf düğümlerinde şişlikler ve sistemik belirtiler gibi çeşitli semptomlarla karakterizedir.',
+  'Bacak': 'Periferik arter hastalığı, vücudun dışındaki (periferik) arterlerdeki daralma veya tıkanma sonucu oluşan bir durumdur. Bu durum, bacaklarda ağrı, uyuşma, yürüme güçlüğü veya kangren gibi semptomlarla karakterizedir.',
+  'Diz': 'Kireçlenme, eklemlerdeki kıkırdak dokusunun zamanla aşınması ve yıpranması sonucu ortaya çıkan bir durumdur. Bu durum, dizlerde ağrı, şişlik ve hareket kısıtlılığına neden olabilir.',
+  'Ön Ayak': 'Medikal kollateral ligament (MCL) yaralanmaları, dizin iç kısmındaki ligamentin yaralanması sonucu oluşan bir durumdur. Bu yaralanmalar genellikle dizin içe doğru bükülmesi veya dışa doğru zorlanması sonucu meydana gelir ve dizde ağrı, şişlik ve stabilite kaybına neden olabilir.',
+  'Ayak': 'Topuk dikeni, topuk kemiğinin altında, tabanı destekleyen lifli bir bağın kopmasına neden olan bir durumdur. Bu durum, topuk altında keskin ve şiddetli ağrı ile karakterizedir, özellikle ilk adımda veya uzun süre ayakta kaldıktan sonra artabilir.'
 };
 
 // Menü butonu ve içeriği
@@ -118,23 +150,21 @@ for (const category in bodyParts) {
 
   const sublist = document.createElement('div');
   sublist.classList.add('sublist');
+  sublist.style.display = 'none'; // İlk başta gizli olarak ayarla
 
-  bodyParts[category].forEach(part => {
+  bodyParts[category].subparts.forEach(part => {
       const partButton = document.createElement('button');
       partButton.textContent = part;
       partButton.classList.add('part-button');
-      partButton.style.display = 'none'; // İlk başta gizli olarak ayarla
-      sublist.appendChild(partButton);
-
-      // Part butonuna tıklandığında
       partButton.addEventListener('click', () => {
         const selectedPart = partButton.textContent;
-        infoText.textContent = `Seçilen parça: ${selectedPart}`;
+        infoText.textContent = partDescriptions[selectedPart];
         infoBox.style.right = '10px'; // Kayan kutuyu ekrana getir
       });
+      sublist.appendChild(partButton);
   });
 
-  // Kategori butonuna tıklandığında alt menüyü göster
+  // Kategori butonuna tıklandığında alt menüyü göster/gizle
   categoryButton.addEventListener('click', () => {
     // Tüm alt menüleri gizle
     const allSublists = document.querySelectorAll('.sublist');
@@ -143,6 +173,8 @@ for (const category in bodyParts) {
     });
     // Seçilen kategori altındaki alt menüyü göster
     sublist.style.display = 'block';
+    infoText.textContent = bodyParts[category].content;
+    infoBox.style.right = '10px'; // Kayan kutuyu ekrana getir
   });
 
   menuContent.appendChild(sublist);
